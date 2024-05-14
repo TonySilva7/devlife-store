870f8e7a157e09857edc05a443ce55591f0b4bdc
TonySilva7 <tony.deve7oper@gmail.com>
2024-05-14 10:22:09 -0300

    feat: implement search params

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -1,8 +1,9 @@
 import Image from 'next/image'
+import { Metadata } from 'next'
 
 import { api } from '@app/data/api'
 import { Product } from '@app/data/types/product'
-import { Metadata } from 'next'
+import { AddToCartButton } from '@app/components/add-to-cart-button'
 
 interface ProductProps {
   params: {
@@ -43,6 +44,7 @@ export async function generateStaticParams() {
 
 export default async function ProductPage({ params }: ProductProps) {
   const product = await getProduct(params.slug)
+
   return (
     <div className="relative grid max-h-[860px] grid-cols-3">
       <div className="col-span-2 overflow-hidden">
@@ -58,7 +60,9 @@ export default async function ProductPage({ params }: ProductProps) {
       <div className="flex flex-col justify-center px-12">
         <h1 className="text-3xl font-bold leading-tight">{product.title}</h1>
 
-        {product.description}
+        <p className="mt-2 leading-relaxed text-zinc-400">
+          {product.description}
+        </p>
 
         <div className="mt-8 flex items-center gap-3">
           <span className="inline-block rounded-full bg-violet-500 px-5 py-2.5 font-semibold">
@@ -70,12 +74,46 @@ export default async function ProductPage({ params }: ProductProps) {
             })}
           </span>
           <span className="text-sm text-zinc-400">
+            Em até 12x s/ juros de{' '}
             {(product.price / 12).toLocaleString('pt-BR', {
               style: 'currency',
               currency: 'BRL',
             })}
           </span>
         </div>
+
+        <div className="mt-8 space-y-4">
+          <span className="block font-semibold">Tamanhos</span>
+
+          <div className="flex gap-2">
+            <button
+              type="button"
+              className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold"
+            >
+              P
+            </button>
+            <button
+              type="button"
+              className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold"
+            >
+              M
+            </button>
+            <button
+              type="button"
+              className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold"
+            >
+              G
+            </button>
+            <button
+              type="button"
+              className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold"
+            >
+              GG
+            </button>
+          </div>
+        </div>
+
+        <AddToCartButton productId={product.id} />
       </div>
     </div>
   )
